066922f1d3a7315cb632491ada0a20d5ef974ec6
yamee-dev <ryo.eiyama@icloud.com>
2019-11-09 23:50:54 +0900

    axiosを導入

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <myheader></myheader>
+    <p v-if="msg.length > 0">{{msg}}</p>
+    <p v-else>no text</p>
+    <input type="text" v-model="msg" />
+    <button @click="clear()">clear</button>
+    <ul>
+      <li v-for="(user, key) in users" :key="key">{{ user.name }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import myheader from "./components/myheader";
+import axios from "axios";
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    myheader
+  },
+  data() {
+    return {
+      msg: "Hello World",
+      users: []
+    };
+  },
+  methods: {
+    clear() {
+      this.msg = "";
+    }
+  },
+  mounted: function() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then(response => (this.users = response.data));
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/myheader.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>ここはヘッダー</div>
+</template>
